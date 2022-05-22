792e858724b5cb69102a8431315a528c2eefff42
Renato Serra <renato-serra@hotmail.com>
2022-05-22 14:14:35 +0100

    add Seed

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,4 +2,8 @@
 
 class Vehicle < ApplicationRecord
   belongs_to :model
+
+  enum status: {
+    available: 0, action_required: 1, action_in_progress: 2
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+brands = Brand.create([{ name: 'Honda' }, { name: 'Yamaha' }, { name: 'Zoomo' }, { name: 'nothng' }])
+
+models = Model.create([{ name: 'Cx 1', brand: brands[rand(0..3)] }, { name: 'PP 50', brand: brands[rand(0..3)] }, { name: 'L99', brand: brands[rand(0..3)] }, { name: '42', brand: brands[rand(0..3)] }, { name: 'Best', brand: brands[rand(0..3)] }, { name: 'Lower', brand: brands[rand(0..3)] }])
+
+(0..49).each do |i|
+  Vehicle.create(
+    name: "Vehicle #{i}",
+    legal_identifier: i.to_s,
+    frame_size: 0..100,
+    status: rand(0..2),
+    model: models[rand(0..5)]
+  )
+end
